6b2c81cddff16d4a9dcbbe0fe9db164f1c2023dd
magnusisvensson <magnus.i.svensson@lnu.se>
2026-06-17 13:57:09 +0200

    Update vfu_app.py

--- vfu_app.py
+++ vfu_app.py
@@ -1,9 +1,9 @@
 
-import streamlit as st
- openpyxl styles import Font, PatternFill, Alignmentimport pandas as pd
+import streamlit as stimport streamimport pandas as pd
 
 from openpyxl import Workbook
-
+from openpyxl.styles import Font, PatternFill, Alignment
+``
 
 st.title("VFU-placeringssystem")
 
